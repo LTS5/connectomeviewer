d7a8bc91a6608f63086c7c5ff9a393b6731a30b8
Stephan Gerhard <git@unidesign.ch>
2010-04-29 15:12:44 +0200

    Updated Fedora Script

--- scripts/install_cviewer_fedora.sh
+++ scripts/install_cviewer_fedora.sh
@@ -1,18 +1,50 @@
-su
+echo "Type the full path to store ConnectomeViewer and Enthought Toolstack Source Files:"
+read fullp
+mkdir $fullp
+cd $fullp
 
-yum remove python-AppTools python-EnvisageCore python-EnvisagePlugins python-Traits python-TraitsBackendQt python-TraitsGUI
+echo "Type username to download ConnectomeViewer:"
+read usern
+echo "Type password to download ConnecctomeViewer:"
+read passw
 
-yum update python
-yum install python-pip
-yum install numpy scipy python-devel ipython
-pip install ETSProjectTools
-ets co ETS
-yum install vtk vtk-devel swig python-configobj libXtst libXtst-devel freeglut freeglut-devel python-matplotlib python-lxml Cython qscintilla-python python-sphinx wxPython wxPython-devel subversion gcc gcc-c++ libXt libXt-devel libX11-devel
+# This removes Ubuntu Packages with are not uptodate
+sudo yum remove python-AppTools python-EnvisageCore python-EnvisagePlugins python-Traits python-TraitsBackendQt python-TraitsGUI
 
-ets install
-pip install networkx
+# This adds required packages, including header files for compilation
+sudo yum update python
+yum install python-pip numpy scipy python-devel ipython vtk vtk-devel swig python-configobj libXtst libXtst-devel freeglut freeglut-devel python-matplotlib python-lxml Cython qscintilla-python python-sphinx wxPython wxPython-devel subversion gcc gcc-c++ libXt libXt-devel libX11-devel
 
-wget http://unidesign.ch/neuron/ConnectomeViewer-0.1.6.tar.gz
+# We need the Enthought Toolstack
+sudo pip install ETSProjectTools
+
+# You can also directly download an archive
+wget --user=$usern --password=$passw  http://www.connectomeviewer.org/files/ETS_3.3.1.tar.gz
+mkdir ETS
+cd ETS
+tar xzf ETS_3.3.1.tar.gz
+sudo ets develop
+cd ..
+
+# Now we install required packages for the ConnectomeViewer using pip
+# You can easily uninstall them
+sudo easy_install networkx
+sudo pip install h5py
+
+# To access MySQL databases, we need PyMySQL
+sudo easy_install http://pymysql.googlecode.com/files/PyMySQL-0.2-py2.6.egg
+
+# Finally, install the ConnectomeViewer
+wget --user=$usern --password=$passw http://www.connectomeviewer.org/files/ConnectomeViewer-0.1.6.tar.gz
 tar xzf ConnectomeViewer-0.1.6.tar.gz
-cd ConnectomeViewer-0.1.6/
-python setup.py install
+cd ConnectomeViewer-0.1.6
+sudo python setup.py install
+cd ..
+# Download Example datasets
+mkdir ExampleDatasets
+cd ExampleDatasets
+wget http://www.connectomeviewer.org/datasets/homo_sapiens_01.cff
+wget http://www.connectomeviewer.org/datasets/homo_sapiens_02.cff
+wget http://www.connectomeviewer.org/datasets/homo_sapiens_03.cff
+wget http://www.connectomeviewer.org/datasets/macaca_mulatta_01.cff
+cd ..
