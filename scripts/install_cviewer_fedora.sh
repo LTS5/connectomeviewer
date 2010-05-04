58464bf0ca454db27e99dd622e3dc813530b009d
Stephan Gerhard <git@unidesign.ch>
2010-05-04 18:10:48 +0200

    Updated installation scripts

--- scripts/install_cviewer_fedora.sh
+++ scripts/install_cviewer_fedora.sh
@@ -1,46 +1,78 @@
-echo "Type the full path to store ConnectomeViewer and Enthought Toolstack Source Files:"
+echo "Welcome to the Installation Script for ConnectomeViewer"
+echo "======================================================="
+echo "Type the full path to store ConnectomeViewer and Enthought Toolsuite Source Files:"
 read fullp
 mkdir $fullp
 cd $fullp
 
-echo "Type username to download ConnectomeViewer:"
-read usern
-echo "Type password to download ConnectomeViewer:"
-read passw
-
-# This removes Ubuntu Packages with are not uptodate
+echo "============================================="
+echo "Remove Fedora Packages which are not uptodate"
+echo "============================================="
 sudo yum remove python-AppTools python-EnvisageCore python-EnvisagePlugins python-Traits python-TraitsBackendQt python-TraitsGUI
 
-# This adds required packages, including header files for compilation
+echo "===================================================================="
+echo "Add required Fedora packages, including header files for compilation"
+echo "===================================================================="
 sudo yum update python
 yum install python-pip numpy scipy python-devel ipython vtk vtk-devel swig python-configobj libXtst libXtst-devel freeglut freeglut-devel python-matplotlib python-lxml Cython qscintilla-python python-sphinx wxPython wxPython-devel subversion gcc gcc-c++ libXt libXt-devel libX11-devel hdf5 hdf5-devel
 
-# We need the Enthought Toolstack
+echo "==============================="
+echo "We need the Enthought Toolsuite"
+echo "==============================="
 sudo pip install ETSProjectTools
 
-# You can also directly download an archive
+# currently includes
+# "ETS==3.3.1"
+# "SciMath['traits']==3.0.5" "Enable['traits']==3.2.1" "Chaco==3.2.1"
+# "ETSDevTools==3.0.4" "EnvisageCore==3.1.2" "TraitsBackendQt==3.2.1"
+# "ETSProjectTools==0.6.0" "TraitsBackendWX==3.2.1" "Mayavi==3.3.1"
+# "EnvisagePlugins==3.1.2" "BlockCanvas==3.1.1" "EnthoughtBase['traits', 'distribution', 'ui']==3.0.4"
+# "TraitsGUI['dock']==3.1.1" "SetupDocs==1.0.4" "Traits['ui']==3.2.1" "CodeTools==3.1.1" "AppTools==3.3.1"
+
 mkdir ETS
 cd ETS
-wget --user=$usern --password=$passw  http://www.connectomeviewer.org/files/ETS_3.3.1.tar.gz
+wget http://www.connectomeviewer.org/files/ETS_3.3.1.tar.gz
 tar xzf ETS_3.3.1.tar.gz
-sudo ets develop
+sudo ets install
 cd ..
+echo "============================================================================================="
+echo "Enthought Toolsuite Download, Compilation and Installation completed (There might be errors)."
+echo "============================================================================================="
+echo "Install some required packages for the ConnectomeViewer"
+echo "======================================================="
 
-# Now we install required packages for the ConnectomeViewer using pip
-# You can easily uninstall them
+echo "============"
+echo "... NetworkX"
+echo "============"
 sudo easy_install networkx
+
+echo "=========="
+echo "... Cython"
+echo "=========="
+sudo easy_install cython
+
+echo "==============="
+echo "... h5py (HDF5)"
+echo "==============="
 sudo pip install h5py
 
-# To access MySQL databases, we need PyMySQL
+echo "==========="
+echo "... PyMySQL"
+echo "==========="
 sudo easy_install http://pymysql.googlecode.com/files/PyMySQL-0.2-py2.6.egg
 
-# Finally, install the ConnectomeViewer
-wget --user=$usern --password=$passw http://www.connectomeviewer.org/files/ConnectomeViewer-0.1.6.tar.gz
+echo "========================================="
+echo "Download and install the ConnectomeViewer"
+echo "========================================="
+wget http://www.connectomeviewer.org/files/ConnectomeViewer-0.1.6.tar.gz
 tar xzf ConnectomeViewer-0.1.6.tar.gz
 cd ConnectomeViewer-0.1.6
 sudo python setup.py install
 cd ..
-# Download Example datasets
+
+echo "========================================="
+echo "Download Connectome File Example datasets"
+echo "========================================="
 mkdir ExampleDatasets
 cd ExampleDatasets
 wget http://www.connectomeviewer.org/datasets/homo_sapiens_01.cff
@@ -48,3 +80,13 @@ wget http://www.connectomeviewer.org/datasets/homo_sapiens_02.cff
 wget http://www.connectomeviewer.org/datasets/homo_sapiens_03.cff
 wget http://www.connectomeviewer.org/datasets/macaca_mulatta_01.cff
 cd ..
+
+echo "==================================================="
+echo "The script went through. Errors might have occured."
+echo ""
+echo "Try to start ConnectomeViewer with typing:"
+echo " connectomeviewer -v"
+echo "If the ConnectomeViewer does not work properly, please re-run this installation script with:"
+echo " sh ./install_cviewer_fedora.sh > logfile.txt"
+echo "and send the logfile.txt to info@connectomics.org. Thank you!"
+echo "============================================================="
--- scripts/install_cviewer_ubuntu.sh
+++ scripts/install_cviewer_ubuntu.sh
@@ -1,68 +1,77 @@
-echo "Type the full path to store ConnectomeViewer and Enthought Toolstack Source Files:"
+echo "Welcome to the Installation Script for ConnectomeViewer"
+echo "======================================================="
+echo "Type the full path to store ConnectomeViewer and Enthought Toolsuite Source Files:"
 read fullp
 mkdir $fullp
 cd $fullp
 
-echo "Type username to download ConnectomeViewer:"
-read usern
-echo "Type password to download ConnectomeViewer:"
-read passw
-
-# This removes Ubuntu Packages with are not uptodate
+echo "============================================="
+echo "Remove Ubuntu Packages which are not uptodate"
+echo "============================================="
 sudo apt-get remove mayavi2 python-enthoughtbase python-enthought-chaco2 python-enthought-traits python-enthought-traits-ui python-apptools python-chaco python-envisageplugins python-networkx
 
-# This adds required packages, including header files for compilation
-sudo apt-get install subversion python-setuptools libvtk5.2 python-vtk python-numpy python-wxversion python2.6-dev python-sphinx g++ swig python-configobj glutg3 glutg3-dev libxtst-dev ipython python-lxml python-matplotlib python-qscintilla2 cython gcc scons python-xlib pyqt4-dev-tools python-scipy python-pyrex python-all-dev libxt-dev libglu1-mesa-dev python-pip wget
+echo "===================================================================="
+echo "Add required Ubuntu packages, including header files for compilation"
+echo "===================================================================="
+sudo apt-get install subversion python-setuptools libvtk5.2 python-vtk python-numpy python-wxversion python2.6-dev python-sphinx g++ swig python-configobj glutg3 glutg3-dev libxtst-dev ipython python-lxml python-matplotlib python-qscintilla2 gcc scons python-xlib pyqt4-dev-tools python-scipy python-pyrex python-all-dev libxt-dev libglu1-mesa-dev python-pip wget
 
-# We need the Enthought Toolstack
+echo "==============================="
+echo "We need the Enthought Toolsuite"
+echo "==============================="
 sudo pip install ETSProjectTools
 
-# You can also checkout the current version of ETS with:
-#  ets co ETS
-# This takes some time...please be patient
-# The following projects are checked out
-# Checking out "ETS==3.3.1" source from "https://svn.enthought.com/svn/enthought/ETS/trunk"
-# Checking out "SciMath['traits']==3.0.5" source from "https://svn.enthought.com/svn/enthought/SciMath/trunk"
-# Checking out "Enable['traits']==3.2.1" source from "https://svn.enthought.com/svn/enthought/Enable/trunk"
-# Checking out "Chaco==3.2.1" source from "https://svn.enthought.com/svn/enthought/Chaco/trunk"
-# Checking out "ETSDevTools==3.0.4" source from "https://svn.enthought.com/svn/enthought/ETSDevTools/trunk"
-# Checking out "EnvisageCore==3.1.2" source from "https://svn.enthought.com/svn/enthought/EnvisageCore/trunk"
-# Checking out "TraitsBackendQt==3.2.1" source from "https://svn.enthought.com/svn/enthought/TraitsBackendQt/trunk"
-# Checking out "ETSProjectTools==0.6.0" source from "https://svn.enthought.com/svn/enthought/ETSProjectTools/trunk"
-# Checking out "TraitsBackendWX==3.2.1" source from "https://svn.enthought.com/svn/enthought/TraitsBackendWX/trunk"
-# Checking out "Mayavi==3.3.1" source from "https://svn.enthought.com/svn/enthought/Mayavi/trunk"
-# Checking out "EnvisagePlugins==3.1.2" source from "https://svn.enthought.com/svn/enthought/EnvisagePlugins/trunk"
-# Checking out "BlockCanvas==3.1.1" source from "https://svn.enthought.com/svn/enthought/BlockCanvas/trunk"
-# Checking out "EnthoughtBase['traits', 'distribution', 'ui']==3.0.4" source from "https://svn.enthought.com/svn/enthought/EnthoughtBase/trunk"
-# Checking out "TraitsGUI['dock']==3.1.1" source from "https://svn.enthought.com/svn/enthought/TraitsGUI/trunk"
-# Checking out "SetupDocs==1.0.4" source from "https://svn.enthought.com/svn/enthought/SetupDocs/trunk"
-# Checking out "Traits['ui']==3.2.1" source from "https://svn.enthought.com/svn/enthought/Traits/trunk"
-# Checking out "CodeTools==3.1.1" source from "https://svn.enthought.com/svn/enthought/CodeTools/trunk"
-# Checking out "AppTools==3.3.1" source from "https://svn.enthought.com/svn/enthought/AppTools/trunk"
+# currently includes
+# "ETS==3.3.1"
+# "SciMath['traits']==3.0.5" "Enable['traits']==3.2.1" "Chaco==3.2.1"
+# "ETSDevTools==3.0.4" "EnvisageCore==3.1.2" "TraitsBackendQt==3.2.1"
+# "ETSProjectTools==0.6.0" "TraitsBackendWX==3.2.1" "Mayavi==3.3.1"
+# "EnvisagePlugins==3.1.2" "BlockCanvas==3.1.1" "EnthoughtBase['traits', 'distribution', 'ui']==3.0.4"
+# "TraitsGUI['dock']==3.1.1" "SetupDocs==1.0.4" "Traits['ui']==3.2.1" "CodeTools==3.1.1" "AppTools==3.3.1"
 
-# You can also directly download an archive
 mkdir ETS
 cd ETS
-wget --user=$usern --password=$passw  http://www.connectomeviewer.org/files/ETS_3.3.1.tar.gz
-tar xzf ETS_3.3.1.tar.gz
-sudo ets develop
+wget http://www.connectomeviewer.org/files/ETS_3.3.1.tar.gz
+tar zxvf ETS_3.3.1.tar.gz
+sudo ets install
 cd ..
+echo "============================================================================================="
+echo "Enthought Toolsuite Download, Compilation and Installation completed (There might be errors)."
+echo "============================================================================================="
+echo "Install some required packages for the ConnectomeViewer"
+echo "======================================================="
 
-# Now we install required packages for the ConnectomeViewer using pip
-# You can easily uninstall them
+echo "============"
+echo "... NetworkX"
+echo "============"
 sudo easy_install networkx
+
+echo "=========="
+echo "... Cython"
+echo "=========="
+sudo easy_install cython
+
+echo "==============="
+echo "... h5py (HDF5)"
+echo "==============="
 sudo pip install h5py
 
-# To access MySQL databases, we need PyMySQL
+echo "==========="
+echo "... PyMySQL"
+echo "==========="
 sudo easy_install http://pymysql.googlecode.com/files/PyMySQL-0.2-py2.6.egg
 
-# Finally, install the ConnectomeViewer
-wget --user=$usern --password=$passw http://www.connectomeviewer.org/files/ConnectomeViewer-0.1.6.tar.gz
+echo "========================================="
+echo "Download and install the ConnectomeViewer"
+echo "========================================="
+wget http://www.connectomeviewer.org/files/ConnectomeViewer-0.1.6.tar.gz
 tar xzf ConnectomeViewer-0.1.6.tar.gz
 cd ConnectomeViewer-0.1.6
 sudo python setup.py install
 cd ..
-# Download Example datasets
+
+echo "========================================="
+echo "Download Connectome File Example datasets"
+echo "========================================="
 mkdir ExampleDatasets
 cd ExampleDatasets
 wget http://www.connectomeviewer.org/datasets/homo_sapiens_01.cff
@@ -71,3 +80,12 @@ wget http://www.connectomeviewer.org/datasets/homo_sapiens_03.cff
 wget http://www.connectomeviewer.org/datasets/macaca_mulatta_01.cff
 cd ..
 
+echo "==================================================="
+echo "The script went through. Errors might have occured."
+echo ""
+echo "Try to start ConnectomeViewer with typing:"
+echo " connectomeviewer -v"
+echo "If the ConnectomeViewer does not work properly, please re-run this installation script with:"
+echo " sh ./install_cviewer_ubuntu.sh > logfile.txt"
+echo "and send the logfile.txt to info@connectomics.org. Thank you!"
+echo "============================================================="
