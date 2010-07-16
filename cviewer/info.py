17e87cf30d9ac6118eb0dfde585c8115f68ace90
Stephan Gerhard <git@unidesign.ch>
2010-07-16 09:56:07 +0200

    Update version dependencies for Ubuntu 10.04

--- cviewer/info.py
+++ cviewer/info.py
@@ -57,6 +57,32 @@ h5py_min_version = '1.2.0'
 scipy_min_version = '0.5'
 cython_min_version = '0.12'
 
+# for ubuntu 10.04
+###
+envisagecore_min_version = '3.1.1' # python-envisagecore
+envisageplugins_min_version = '3.1.1' # python-envisageplugins
+
+traitsbackendwx_min_version = '3.2.0' # python-traitsbackendwx
+traitsbackendqt_min_version = '3.2.0' # python-traitsbackendqt
+traitsgui_min_version = '3.1.0' # python-traitsgui
+traits_min_version = '3.2.0' # python-traits
+
+python_enthought_traits_ui = '3.1.0' # python-enthought-traits-ui
+enthoughtbase_min_version = '3.0.3' # python-enthoughtbase
+chaco_min_version = '3.2.0' # python-chaco
+lxml_min_version = '2.2.4' # python-lxml
+scipy_min_version = '0.5' # python-scipy (0.7.0)
+h5py_min_version = '1.2.1' # python-h5py
+mayavi_min_version = '3.3.0' # mayavi2
+
+# python-apptools (3.3.0), python-enthought-traits (3.1.0), python-numpy (1.3.0)
+
+# outdated or not available
+pymysql_min_version = '0.2' 
+networkx_min_version = '1.0' # is out-dated
+cython_min_version = '0.12' # cython (0.11.2)
+
+
 # dipy_min_version = ''
 # nibabel_min_version = ''
 # nipy_min_version = ''
