3b17301405c11beba1fa343a4305d50d2be8dd35
Stephan Gerhard <git@unidesign.ch>
2010-10-27 19:03:15 +0100

    BF - Removed SphinxDoc builder

--- build_helpers.py
+++ build_helpers.py
@@ -104,56 +104,6 @@ def relative_path(filename):
 
 
 ################################################################################
-# Distutils Command class build the docs 
-class MyBuildDoc(BuildDoc):
-    """ Sub-class the standard sphinx documentation building system, to
-        add logics for API generation and matplotlib's plot directive.
-    """
-
-    def run(self):
-        self.run_command('api_docs')
-        # We need to be in the doc directory for to plot_directive
-        # and API generation to work
-        os.chdir('doc')
-        try:
-            BuildDoc.run(self)
-        finally:
-            os.chdir('..')
-        self.zip_docs()
-    
-    def zip_docs(self):
-        if not os.path.exists(DOC_BUILD_DIR):
-            raise OSError, 'Doc directory does not exist.'
-        target_file = os.path.join('doc', 'documentation.zip')
-        # ZIP_DEFLATED actually compresses the archive. However, there
-        # will be a RuntimeError if zlib is not installed, so we check
-        # for it. ZIP_STORED produces an uncompressed zip, but does not
-        # require zlib.
-        try:
-            zf = zipfile.ZipFile(target_file, 'w', 
-                                        compression=zipfile.ZIP_DEFLATED)
-        except RuntimeError:
-            warnings.warn('zlib not installed, storing the docs '
-                            'without compression')
-            zf = zipfile.ZipFile(target_file, 'w', 
-                                        compression=zipfile.ZIP_STORED)    
-
-        for root, dirs, files in os.walk(DOC_BUILD_DIR):
-            relative = relative_path(root)
-            if not relative.startswith('.doctrees'):
-                for f in files:
-                    zf.write(os.path.join(root, f), 
-                            os.path.join(relative, 'html_docs', f))
-        zf.close()
-
-
-    def finalize_options(self):
-        """ Override the default for the documentation build
-            directory.
-        """
-        self.build_dir = os.path.join(*DOC_BUILD_DIR.split(os.sep)[:-1])
-        BuildDoc.finalize_options(self)
-
 
 ################################################################################
 # Distutils Command class to clean
@@ -170,7 +120,7 @@ class Clean(clean):
             shutil.rmtree(DOC_BUILD_DIR)
 
 # The command classes for distutils, used by the setup.py
-cmdclass = {'build_sphinx': MyBuildDoc,
+cmdclass = {#'build_sphinx': MyBuildDoc,
 #            'api_docs': APIDocs,
             'clean': Clean,
             }
