4930acae8a632d8488bd90b2a59d33d76c33bc7a
Stephan Gerhard <git@unidesign.ch>
2010-12-05 11:20:20 +0100

    Updated hdf5 ideas

--- scratch/mmhd.py
+++ scratch/mmhd.py
@@ -44,3 +44,43 @@
         - areatree
         - 2d polygons. closed and open.
         
+principle: separation between semantics and geometry
+
+open:
+- atlas vs. individual
+- multiple surface representation vs. canonical
+- store fibers?
+- brainregion: region or regionset?
+
+/brainregions
+    /region <of class, id>
+        /volume
+        /mesh
+        /slices (with polygons)
+        
+    /regionset <of class, id>
+        
+        
+/brainregionconnections
+    /connectionmatrix (subgraph of connections)
+        /mapping row index to region id
+        /matrix
+    /fibergroup (tractography)
+
+/cells
+    /cellgroup <of class, id> -> NeuronGroup / CellGroup Actor
+        /cell-basic-geometry-types:
+        /skeleton: pointset, connectivity, radius, confidence
+        /mesh: pointset, faces
+        /volume: e.g. labeling indices
+        /areatree: closed or open polygons
+        /sphere: point, radius
+        -> all in contiguous array and storing corresponding integer ids (as offsets)  
+        
+/synapses
+    /synapsegroup <of class, id>
+        /synapse-basic-geometry-types:
+        /connector: one-to-many / many-to-many -> store ids from cells
+        /polygon (e.g. for cleft area)
+        
+        
