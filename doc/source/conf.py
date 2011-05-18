dac17bda317ad15a440c7548d0e73ea7dd35e913
Stephan Gerhard <git@unidesign.ch>
2011-05-18 16:05:13 +0200

    Update docs

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -72,9 +72,9 @@ copyright = '2010, EPFL & UNIL-CHUV. Author: Stephan Gerhard'
 # other places throughout the built documents.
 #
 # The short X.Y version.
-version = '2.0.0 RC2'
+version = '2.0.0 RC4'
 # The full version, including alpha/beta/rc tags.
-release = '2.0.0 RC2'
+release = '2.0.0 RC4'
 
 # There are two options for replacing |today|: either, you set today to some
 # non-false value, then it is used:
